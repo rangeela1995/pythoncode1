9f3a9bc0f28bfb701c5bbbdbad8ae8e918a23ce8
Rangeela P <90337292+rangeela1995@users.noreply.github.com>
2021-10-14 16:57:53 +0530

    round1.py

---
+++ round1.py
@@ -0,0 +1,23 @@
+a = list(map(int,input("\nEnter the elements for array a: ").strip().split()))
+print(a)
+n1 = int(input('Enter a number: '))
+
+def function1(n1,a) :
+ length = len(a)
+ if n1 in a:     
+   for i in range (length):
+        if a[i] == n1:
+           print("Index of",n1,"is",i)
+ if n1 not in a:
+    for i in range (length):
+        if a[i] > n1:
+          index = i
+          print("Index of",n1,"is",i)
+          break
+
+ if n1 > a[length-1]:
+    index = length
+    print("Index of",n1,"is",i)
+function1(n1,a)
+          
+          
